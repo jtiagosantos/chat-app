1fea0deb82210557a2ac1e5bdc3f899df3a9aad5
jtiagosantos <tiagosan040@gmail.com>
2022-05-15 19:10:01 -0300

    feat: 🎸 put Chat page on routes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes as Router, Route } from "react-router-dom";
 
 //pages
 import { Home } from "../pages/Home/Home";
+import { Chat } from "../pages/Chat/Chat";
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <Router>
         <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
       </Router>
     </BrowserRouter>
   );
