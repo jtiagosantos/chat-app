09e43204fd2bbd25eca3d8fa3672ef928444783f
jtiagosantos <tiagosan040@gmail.com>
2022-05-23 23:58:48 -0300

    chore: create new Home page

--- web/src/pages/Home/Home.tsx
+++ web/src/pages/Home/Home.tsx
@@ -1,70 +1,33 @@
-import { FormEvent, useState } from 'react';
-import { useNavigate, createSearchParams } from 'react-router-dom';
-
-//constants
-import { constants } from '../../constants';
+import { useNavigate } from 'react-router-dom';
 
 //styles
 import { Container } from './styles';
 
 export const Home = () => {
   const navigate = useNavigate();
-  const { USER_IMAGE } = constants;
 
-  const [usernameInput, setUsernameInput] = useState<string>('');
-  const [profilePhotoURLInput, setProfilePhotoURLInput] = useState<string>('');
-  
-  const navigateToChatPage = () => {
-    navigate({
-      pathname: '/chat',
-      search: createSearchParams({
-        username: usernameInput,
-        profile_photo: profilePhotoURLInput,
-      }).toString(),
-    })
+  const navigateToCreateRoomPage = () => {
+    navigate('/room/create');
   }
 
-  const handleSubmitForm = (event: FormEvent) => {
-    event.preventDefault();
-
-    if (!profilePhotoURLInput) {
-      alert('profile photo URL is required');
-      return;
-    }
-
-    if (!usernameInput) {
-      alert('username field is required');
-      return;
-    }
-
-    navigateToChatPage();
+  const navigateToEnterRoomPage = () => {
+    navigate('/room/enter');
   }
 
   return (
     <Container>
-      <div>
-        <img src={profilePhotoURLInput || USER_IMAGE.DEFAULT} alt="not found" />
-      </div>
-
-      <form onSubmit={handleSubmitForm}>
-        <input 
-          type="text" 
-          placeholder="Insert your profile photo URL" 
-          value={profilePhotoURLInput}
-          onChange={
-            ({ target }) => setProfilePhotoURLInput(target.value)
-          }
-        />
-        <input 
-          type="text" 
-          placeholder="Insert your username" 
-          value={usernameInput}
-          onChange={
-            ({ target }) => setUsernameInput(target.value)
-          }
-        />
-        <button type="submit">Enter on chat</button>
-      </form>
+      <button 
+        type="button" 
+        onClick={navigateToCreateRoomPage}
+      >
+        Create a Room
+      </button>
+      <button 
+        type="button" 
+        onClick={navigateToEnterRoomPage}
+      >
+        Enter a Room
+      </button>
     </Container>
   );
 }
--- web/src/pages/Home/styles.ts
+++ web/src/pages/Home/styles.ts
@@ -5,61 +5,25 @@ export const Container = styled.main`
   height: 100vh;
 
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 1.2rem;
+  gap: 1.5rem;
 
-  div img {
-    max-width: 6.25rem;
-    height: 6.25rem;
-    width: 100%;
-    border-radius: 100%;
-    color: #fff;
-  }
-
-  form {
+  button {
     max-width: 18.75rem;
     width: 100%;
-    margin-top: 20px;
+    height: 3rem;
+    margin-top: 0.5rem;
 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 1rem;
+    border-radius: 8px;
+    background-color: #8257E5;
+    color: #fff;
+    font-size: 1rem;
 
-    input {
-      width: 100%;
-      height: 2.5rem;
-      padding: 0.5rem 0.625rem;
+    transition: filter 0.2ms;
 
-      border: 1px solid gray;
-      border-radius: 8px;
-      background-color: #52525B;
-
-      color: #A1A1AA;
-      font-size: 0.875rem;
-
-      &::placeholder {
-        color: #A1A1AA;
-      }
-    }
-
-    button {
-      max-width: 18.75rem;
-      width: 100%;
-      height: 2.5rem;
-      margin-top: 0.5rem;
-
-      border-radius: 8px;
-      background-color: #8257E5;
-      color: #fff;
-
-      transition: filter 0.2ms;
-
-      &:hover {
-        filter: brightness(0.9);
-      }
+    &:hover {
+      filter: brightness(0.9);
     }
   }
 `;
