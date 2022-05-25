38ad75dd1c8a7afc821cd23e0afe7fe45f15b576
jtiagosantos <tiagosan040@gmail.com>
2022-05-25 18:26:45 -0300

    feat: 🎸 create routes for message

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 
 import { RoomController } from '../controllers/RoomController';
+import { MessageController } from '../controllers/MessageController';
 
 export const routes = Router();
 
 routes.post('/room/create', RoomController.createRoom);
 routes.get('/room/read/:code', RoomController.findUniqueRoomByCode);
+
+routes.post('/message/create', MessageController.createMessage);
+routes.get('/messages/read/:code', MessageController.findMessagesByRoomCode);
