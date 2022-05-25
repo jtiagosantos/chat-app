af26d24721243a8671cc78d2d5be21ac5ee5df81
jtiagosantos <tiagosan040@gmail.com>
2022-05-25 18:26:24 -0300

    feat: 🎸 implement message controller

---
+++ server/src/controllers/MessageController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { MessageRepository } from '../repositories/MessageRepository';
+
+export class MessageController {
+  static async createMessage(req: Request, res: Response) {
+    try {
+      const { body } = req;
+
+      const message = await MessageRepository.create(body);
+
+      res.status(202).json({ data: message });
+    } catch (error: any) {
+      res.status(500).json({
+        data: null,
+        error: error?.message,
+      });
+    }
+  }
+
+  static async findMessagesByRoomCode(req: Request, res: Response) {
+    try {
+      const { code } = req.params;
+
+      const messages = await MessageRepository.read(code);
+
+      res.status(200).json({ data: messages });
+    } catch (error: any) {
+      res.status(500).json({
+        data: null,
+        error: error?.message,
+      });
+    }
+  }
+}
