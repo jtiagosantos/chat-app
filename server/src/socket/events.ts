0b3c581f840f9f55db181ec53e65b805d0086a1a
jtiagosantos <tiagosan040@gmail.com>
2022-05-25 14:57:22 -0300

    feat: 🎸 get online users number by room

--- server/src/socket/events.ts
+++ server/src/socket/events.ts
@@ -1,19 +1,20 @@
 import { constants } from '../constants'
 import { io } from './io';
 import { Message } from '../types/message';
+import { getOnlineUsers } from '../utils/getOnlineUsers';
 
 const { EVENTS } = constants;
 const messages: Array<Message> = [];
-let usersCount = 0;
 
 export const socketEvents = () => {
   const events = io.on('connection', (socket) => {
     const roomCode = socket.handshake.query.roomCode as string;
     const messagesByRoom = messages.filter((message) => message.roomCode === roomCode);
+    let onlineUsers = getOnlineUsers(roomCode);
 
     socket.join(roomCode);
 
-    io.to(roomCode).emit(EVENTS.NEW_USER_CONNECTED, usersCount += 1);
+    io.to(roomCode).emit(EVENTS.NEW_USER_CONNECTED, onlineUsers?.size ?? 1);
     io.to(roomCode).emit(EVENTS.PREVIOUS_MESSAGES, messagesByRoom);
   
     socket.on(EVENTS.SEND_MESSAGE, (message) => {
@@ -22,8 +23,10 @@ export const socketEvents = () => {
     });
 
     socket.on('disconnect', () => {
-      io.to(roomCode).emit(EVENTS.USER_DISCONNECTED, usersCount -= 1);
-    })
+      socket.leave(roomCode);
+      onlineUsers = getOnlineUsers(roomCode);
+      io.to(roomCode).emit(EVENTS.USER_DISCONNECTED, onlineUsers?.size ?? 0);
+    });
   });
 
   return events;
