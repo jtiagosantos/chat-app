baeb68ee009b85ef71982fa1aeaf6077a0d1b8b7
jtiagosantos <tiagosan040@gmail.com>
2022-05-26 14:47:44 -0300

    feat: 🎸 implement service to create message on web

---
+++ web/src/services/message.service.ts
@@ -0,0 +1,21 @@
+import { api } from './api';
+import { Dto } from '../models/Dto';
+import { MessageDto } from '../models/Message';
+import { Message } from '../types/message';
+
+export class MessageService {
+  static async createMessage(message: MessageDto): Promise<Dto<Message>> {
+    try {
+      const { data } = await api.post('/message/create', message);
+
+      return data;
+    } catch (error: any) {
+      const { data } = error.response;
+
+      return {
+        data: null,
+        error: data.error,
+      }
+    }
+  }
+}
