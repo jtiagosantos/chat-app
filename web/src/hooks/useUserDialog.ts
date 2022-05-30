d078c948a14bf165717a3a8525b808d30009ca4f
jtiagosantos <tiagosan040@gmail.com>
2022-05-30 13:45:44 -0300

    feat: 🎸 implement a hook to use UserDialog context

---
+++ web/src/hooks/useUserDialog.ts
@@ -0,0 +1,7 @@
+import { useContextSelector } from 'use-context-selector';
+import { UserDialogContext } from '../contexts/userDialog';
+
+export const useUserDialog = () => useContextSelector(
+  UserDialogContext,
+  (context) => context,
+);
