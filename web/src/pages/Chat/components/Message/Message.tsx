bba8b44e339e549f60b4bdbcd9c8a1ac7adf7c4f
jtiagosantos <tiagosan040@gmail.com>
2022-05-30 13:48:03 -0300

    feat: 🎸 modify Message to open dialog when click in user image

--- web/src/pages/Chat/components/Message/Message.tsx
+++ web/src/pages/Chat/components/Message/Message.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react';
 import dayjs from 'dayjs';
 
+//hooks
+import { useUserDialog } from '../../../../hooks/useUserDialog';
+
 //types
 import { MessageProps } from './types';
 
@@ -13,12 +16,23 @@ export const Message: FC<MessageProps> = ({
   profilePhotoUrl, 
   dateTime 
 }) => {
+  const { setUserData, onOpenDialog } = useUserDialog();
+
   const formattedDatetime = dayjs(dateTime).format('DD/MM/YYYY à[s] HH:mm');
+
+  const handleShowUserDialog = () => {
+    setUserData({ name: username, profileImage: profilePhotoUrl });
+    onOpenDialog();
+  }
 
   return (
     <Container>
       <div>
-        <img src={profilePhotoUrl} alt={username} />
+        <img 
+          src={profilePhotoUrl} 
+          alt={username} 
+          onClick={handleShowUserDialog} 
+        />
         <span>{username}</span>
         <span>{formattedDatetime}</span>
       </div>
