5b120ab6413f5c1768b46920143a3475dcb4379b
jtiagosantos <tiagosan040@gmail.com>
2022-06-01 23:25:09 -0300

    feat: 🎸 implement a method(in user controller) to sign in user

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -46,8 +46,48 @@ export class UserController {
       };
 
       await UserRepository.create(data);
-      
+
       res.end();
+    } catch (error: any) {
+      res.status(500).json({
+        data: null,
+        error: error?.message,
+      });
+    }
+  }
+
+  static async signIn(req: Request, res: Response) {
+    try {
+      const { body } = req;
+
+      const user = await UserRepository.read(body.email);
+
+      if (!user) {
+        res.status(400).json({
+          data: null,
+          error: 'Invalid data',
+        });
+
+        return;
+      }
+
+      const receivedPassword = body.password;
+      const passwordInDatabase = user.password!;
+
+      const areTheSamePasswords = bcrypt.compareSync(receivedPassword, passwordInDatabase);
+
+      if (!areTheSamePasswords) {
+        res.status(400).json({
+          data: null,
+          error: 'Invalid data',
+        });
+
+        return;
+      }
+
+      delete user.password;
+      
+      res.status(200).json({ data: user });
     } catch (error: any) {
       res.status(500).json({
         data: null,
