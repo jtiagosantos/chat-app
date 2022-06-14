12c0e820eb269f9cdf8619b126d6b7bbbfe4d17f
jtiagosantos <tiagosan040@gmail.com>
2022-06-14 12:54:28 -0300

    chore: create theme with colors

---
+++ web/src/styles/theme/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  white: '#ffffff',
+  mediumslateblue: '#8257E5',
+  dimgray: '#52525B',
+  woodsmoke: '#121212',
+  lightgray: '#cccccc',
+  limegreen: '#32ed48',
+  lightgreen: '#5cdb6b',
+  mischka: '#cbd2d9',
+  darkslategray: '#303030',
+  darkgray: '#9aa5b1',
+  overlay: 'rgba(23, 23, 23, 0.7)',
+};
