2a87c91a33d264ba6bd4bcaa8c60eb8176626f8c
jtiagosantos <tiagosan040@gmail.com>
2022-06-17 18:12:16 -0300

    style: 💄 create slideForm animation

---
+++ web/src/styles/animations/slideForm.ts
@@ -0,0 +1,13 @@
+import { keyframes } from 'styled-components';
+
+export const slideForm = keyframes`
+  0% {
+    margin-bottom: -180px;
+    opacity: 0.3;
+  }
+
+  100% {
+    margin-bottom: 0;
+    opacity: 1;
+  }
+`;
