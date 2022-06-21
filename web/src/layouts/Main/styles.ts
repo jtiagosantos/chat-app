a6e0f7ee9effea3dcc6e6814c09ba032d51b0492
jtiagosantos <tiagosan040@gmail.com>
2022-06-21 13:27:17 -0300

    style: 💄 handle responsiveness

--- web/src/layouts/Main/styles.ts
+++ web/src/layouts/Main/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import bannerImage from '@/images/banner.svg';
 
 export const Container = styled.main`
   max-width: 1000px;
@@ -6,11 +7,28 @@ export const Container = styled.main`
   height: 100vh;
   margin: 0 auto;
   padding: 0 1rem;
+  position: relative;
 
   display: flex;
   align-items: center;
   justify-content: space-between;
   gap: 4rem;
+  
+  @media(max-width: 760px) {
+    justify-content: center;
+
+    &::before {
+      content: '';
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      z-index: -1;
+      justify-content: center;
+      background: url(${bannerImage}) no-repeat center;
+      transform: scaleX(-1);
+      opacity: .5;
+    }
+  }
 `;
 
 export const ImageWrapper = styled.div`
@@ -23,5 +41,9 @@ export const ImageWrapper = styled.div`
     width: 100%;
     height: 100%;
     transform: scaleX(-1);
+  }
+
+  @media(max-width: 760px) {
+    display: none;
   }
 `;
