a23d00065273e58aab7f9e477075b25d170f152c
jtiagosantos <tiagosan040@gmail.com>
2022-06-27 15:11:33 -0300

    feat: 🎸 integrate sigInForm component with auth hook

--- web/src/pages/Home/components/SignInForm/SignInForm.tsx
+++ web/src/pages/Home/components/SignInForm/SignInForm.tsx
@@ -1,19 +1,47 @@
 import { FC } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import { useMutation } from 'react-query';
+
+//hooks
+import { useAuthDispatch } from '@/hooks';
 
 //components
 import { Form, Input, Button, PasswordField } from '@/components';
 
+//schemas
+import { signInSchema } from '@/schemas';
+
 //types
-import { SignInFormProps } from './types';
+import { SignInFormProps, FormData } from './types';
 
 //styles
 import * as S from './styles';
 import { theme } from '@/styles/theme';
 
 export const SignInForm: FC<SignInFormProps> = ({ className }) => {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: yupResolver(signInSchema),
+  });
+
+  const { signIn } = useAuthDispatch();
+
+  const { mutate } = useMutation(signIn, {
+    onError: (error) => console.log(error),
+    onSuccess: (data) => console.log(data)
+  });
+
+  const handleSignIn = (data: FormData) => {
+    mutate(data);
+  }
+
   return (
     <S.Container className={className}>
-      <Form width="100%">
+      <Form width="100%" onSubmit={handleSubmit(handleSignIn)}>
         <Input 
           type="email" 
           width="100%"
@@ -23,8 +51,10 @@ export const SignInForm: FC<SignInFormProps> = ({ className }) => {
           placeholderColor={theme.colors.manatee}
           padding="0.5rem 0.625rem"
           placeholder="Insert your e-mail" 
+          control={control}
+          name='email'
         />
-        <PasswordField />  
+        <PasswordField control={control} name='password' />  
 
         <S.Actions>
           <Button
--- web/src/pages/Home/components/SignInForm/types.ts
+++ web/src/pages/Home/components/SignInForm/types.ts
@@ -1,3 +1,8 @@
 export interface SignInFormProps {
   className: 'opening-form' | 'closing-form';
 }
+
+export interface FormData {
+  email: string;
+  password: string;
+}
