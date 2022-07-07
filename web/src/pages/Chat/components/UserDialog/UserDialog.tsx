0e1ba53727bc197f4a45834e0311745fd3395f01
jtiagosantos <tiagosan040@gmail.com>
2022-07-07 13:42:40 -0300

    style: 💄 add animation in userDialog component

--- web/src/pages/Chat/components/UserDialog/UserDialog.tsx
+++ web/src/pages/Chat/components/UserDialog/UserDialog.tsx
@@ -1,4 +1,5 @@
 import { X } from 'phosphor-react';
+import { useTransition } from 'react-spring';
 
 //hooks
 import { useUserDialogDispatch, useUserDialogState } from '@/hooks';
@@ -13,24 +14,32 @@ export const UserDialog = () => {
 
   const { name, profileImage } = user;
 
-  if (isOpenedDialog) {
-    return (
-      <S.Container>
-        <S.Wrapper>
-          <X 
-            size={20} 
-            weight='bold' 
-            color={theme.colors.white}
-            onClick={closeDialog} 
-          />
-        </S.Wrapper>
-        <S.ProfileImageWrapper>
-          <S.ProfileImage src={profileImage} alt={name} />
-        </S.ProfileImageWrapper>
-        <S.Name>{name}</S.Name>
-      </S.Container>
-    );
-  }
+  const dialogTransition = useTransition(isOpenedDialog, {
+    from: { x: -50, y: 0, opacity: 0 },
+    enter: { x: 0, y: 0, opacity: 1 },
+    leave: { x: -50, y: 0, opacity: 0 },
+  });
 
-  return null;
+  return (
+    <>
+      {dialogTransition((style, item) => (
+        item && (
+          <S.Container style={style}>
+            <S.Wrapper>
+              <X 
+                size={20} 
+                weight='bold' 
+                color={theme.colors.white}
+                onClick={closeDialog} 
+              />
+            </S.Wrapper>
+            <S.ProfileImageWrapper>
+              <S.ProfileImage src={profileImage} alt={name} />
+            </S.ProfileImageWrapper>
+            <S.Name>{name}</S.Name>
+          </S.Container>
+        )
+      ))}
+    </>
+  );
 }
--- web/src/pages/Chat/components/UserDialog/styles.ts
+++ web/src/pages/Chat/components/UserDialog/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { animated } from 'react-spring';
 
-export const Container = styled.div`
+export const Container = styled(animated.div)`
   position: absolute;
   left: 30px;
   bottom: -20px;
