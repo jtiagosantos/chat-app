720402ecb375552cfaa24e360cdc43f40bfa9f77
jtiagosantos <tiagosan040@gmail.com>
2022-07-20 21:53:19 -0400

    feat: 🎸 implement shimmer messages loading (fetching messages)

--- web/src/pages/Chat/components/index.ts
+++ web/src/pages/Chat/components/index.ts
@@ -1,3 +1,6 @@
 export { MessageContent } from './MessageContent/MessageContent';
 export { ProfileBar } from './ProfileBar/ProfileBar';
 export { UserDialog } from './UserDialog/UserDialog';
+export { 
+  FetchingMessagesLoading 
+} from './FetchingMessagesLoading/FetchingMessagesLoading';
